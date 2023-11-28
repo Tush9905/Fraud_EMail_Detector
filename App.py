9a750171a94a9d8f5bd9db81bf91f7184006428e
Tush9905 <tush9905@gmail.com>
2023-11-28 22:59:33 +0530

    v1.1
    Used Proper API Calls from Hugging Face Inference API and added more precise stats.

--- App.py
+++ App.py
@@ -1,18 +1,32 @@
 import streamlit as st
 import joblib
+from huggingface_token import token
 
 st.title("Fraud E-Mail Detector")
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/tush9905/email_fraud_detector"
+headers = {"Authorization": f"Bearer {token}"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+
 with st.form("my_form"):
-   email = st.text_input("Copy and Paste the E-Mail Content")
+   email = st.text_input("Copy and Paste the E-Mail Content v1.1")
    submit = st.form_submit_button('Check')
 
    if submit:
-      model = joblib.load("Email_Fraud_Detector_Model.pkl")
-      model_output = model(email)
-      label = model_output[0]["label"]
-      score = model_output[0]["score"]
+      model_output = query({"inputs": email})
+      label = model_output[0][0]["label"]
+      score = model_output[0][0]["score"]
+      label_not = model_output[0][1]["label"]
+      score_not = model_output[0][1]["score"]
       if label == "FRAUD":
         st.error(f"The E-Mail looks {int(score*100)}% {label}")
+        st.write()
       else:
          st.success(f"The E-Mail looks {int(score*100)}% {label}")
-         
+
+      st.write(f"{label} : {score*100}%")
+      st.write(f"{label_not} : {score_not*100}")
